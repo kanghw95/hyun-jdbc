ea652f3c6b8561e71a8fd8f7bdc821b760f13374
hyun <kanghw95@naver.com>
2019-03-22 13:27:44 +0900

    src 올림

---
+++ src/main/java/org/hyun/jdbc/raw/ArticleDao.java
@@ -0,0 +1,15 @@
+package org.hyun.jdbc.raw;
+
+import java.util.List;
+
+public interface ArticleDao {
+List<Article> listArticles();
+
+Article getArticle(String articleId);
+
+void addArticle(Article article);
+
+void updateArticle(Article article);
+
+void deleteArticle(String articleId);
+}
